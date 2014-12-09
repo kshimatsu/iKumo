988fe9a42cd03c4eb94d3a0c2006bf313773dd13
kshimatsu <kristenshimatsu@gmail.com>
2014-12-09 13:56:30 +0800

    Fixing migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141205024901) do
+ActiveRecord::Schema.define(version: 20141209052924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20141205024901) do
     t.string   "gender"
     t.integer  "weight"
     t.integer  "length"
-    t.string   "profile_pic"
+    t.string   "avatar"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
