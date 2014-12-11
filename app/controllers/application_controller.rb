9a1acd166b4fbee1f6bf648893ba65c1cb2ebe67
Fer Martin <fernandomartinsantana@gmail.com>
2014-12-11 17:52:48 +0800

    Cleaning up navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :authenticate_user!, :get_my_babies
+  before_action :authenticate_user!, :babies_list_for_navbar
 
-  def get_my_babies
+  # We use these babies for navbar
+  def babies_list_for_navbar
     @my_babies = current_user.babies if current_user
   end
 end
